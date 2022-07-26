f8f25520fc046f549728fab92ac127c197436028
renata monteiro <rcm@lionx.com.br>
2022-07-26 08:40:29 -0300

    feat repositories and base repository

---
+++ func/src/repositories/job_positions/repository.py
@@ -0,0 +1,54 @@
+# STANDARD IMPORTS
+from typing import List
+
+# PROJECT IMPORTS
+from func.src.domain.employ_positions.model import EmployPositionsResponse
+from func.src.repositories.base_repository.oracle.base import OracleBaseRepository
+from func.src.repositories.cache.repository import EmployPositionsCacheRepository
+
+
+class EmployPositionsRepository:
+
+    @staticmethod
+    def build_employ_positions_model(employ_position: dict) -> EmployPositionsResponse:
+        employ_positions_model = EmployPositionsResponse(
+            code=employ_position.get("CODE"),
+            description=employ_position.get("DESCRIPTION"),
+        )
+
+        return employ_positions_model
+
+    @classmethod
+    def get_employ_positions(cls) -> List[EmployPositionsResponse]:
+        # todo - incluir o nome da tabela
+
+        # sql = """
+        #     SELECT CODE as code, DESCRIPTION as description
+        #     FROM USPIXDB001.XXXXXXXX
+        #     """
+
+        # result = cls._get_employ_cached_enum(query=sql)
+
+        sql_response_stub = [
+            {"CODE": "ADVERTISER", "DESCRIPTION": "ANUNCIANTE"},
+            {"CODE": "AGENT", "DESCRIPTION": "AGENTE"},
+            {"CODE": "ANALYST", "DESCRIPTION": "ANALISTA"}
+        ]
+
+        employ_positions_model = [
+            EmployPositionsRepository.build_employ_positions_model(
+                employ_position=employ_position
+            )
+            for employ_position in sql_response_stub
+        ]
+
+        return employ_positions_model
+
+    @classmethod
+    def _get_employ_cached_enum(cls, query: str) -> list:
+        if cached_enum := EmployPositionsCacheRepository.get_employ_positions_enum():
+            return cached_enum
+
+        enum_values = OracleBaseRepository.query(sql=query)
+        EmployPositionsCacheRepository.save_employ_positions_enum(enum_values)
+        return enum_values
