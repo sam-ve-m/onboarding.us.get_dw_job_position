092fa3d2777b0ce19f4594503ca1643e233dc5bf
renata monteiro <rcm@lionx.com.br>
2022-07-27 13:24:10 -0300

    feat test main

--- func/main.py
+++ func/main.py
@@ -14,6 +14,10 @@ from src.domain.response.model import ResponseModel
 from src.services.employ_positions.service import EmployPositionsService
 
 
+app = Flask(__name__)
+
+
+@app.route('/get_employ_positions')
 async def get_employ_positions(request_body: Request = request) -> Response:
 
     thebes_answer = request_body.headers.get("x-thebes-answer")
@@ -60,3 +64,6 @@ async def get_employ_positions(request_body: Request = request) -> Response:
             message="Something went wrong"
         ).build_http_response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
         return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,38 +10,47 @@ from func.main import get_employ_positions
 from func.src.services.employ_positions.service import EmployPositionsService
 
 # STUB IMPORTS
+from tests.src.employ_positions.stub_service import response_service_stub
 from tests.main_stub import decoded_jwt_stub, stub_employ_stub
 
 
+class Iterable:
+    def __iter__(self):
+        return self
+
+
 @pytest.mark.asyncio
-@patch.object(EmployPositionsService, "get_employ_positions_response", return_value=stub_employ_stub)
+@patch.object()
+@patch.object(EmployPositionsService, "get_employ_positions_response", side_effect=[response_service_stub, {}])
 @patch.object(Heimdall, "decode_payload", return_value=(decoded_jwt_stub, HeimdallStatusResponses.SUCCESS))
 async def test_when_sending_right_params_to_get_employ_positions_then_return_the_expected(
         mock_get_response,
         mock_decode_payload
 ):
+    Iterable.__next__ = mock_get_response
     app = Flask(__name__)
     with app.test_request_context(
             headers=Headers({"x-thebes-answer": "jwt_to_decode_stub"}),
     ).request as request:
+
         response = await get_employ_positions(
             request_body=request
         )
         assert response.status_code == 200
 
 
-@pytest.mark.asyncio
-@patch.object(Heimdall, "decode_payload", return_value=(None, HeimdallStatusResponses.INVALID_TOKEN))
-@patch.object(EmployPositionsService, "get_employ_positions_response", return_value=stub_employ_stub)
-async def test_when_sending_invalid_jwt_to_get_employ_positions_then_raise_error(
-        mock_decode_payload,
-        mock_get_response
-):
-    app = Flask(__name__)
-    with app.test_request_context(
-            headers=Headers({"x-thebes-answer": "jwt_to_decode_stub"}),
-    ).request as request:
-        with pytest.raises(Exception):
-            await get_employ_positions(
-                request_body=None
-            )
+# @pytest.mark.asyncio
+# @patch.object(Heimdall, "decode_payload", return_value=(None, HeimdallStatusResponses.INVALID_TOKEN))
+# @patch.object(EmployPositionsService, "get_employ_positions_response", return_value=stub_employ_stub)
+# async def test_when_sending_invalid_jwt_to_get_employ_positions_then_raise_error(
+#         mock_decode_payload,
+#         mock_get_response
+# ):
+#     app = Flask(__name__)
+#     with app.test_request_context(
+#             headers=Headers({"x-thebes-answer": "jwt_to_decode_stub"}),
+#     ).request as request:
+#         with pytest.raises(Exception):
+#             await get_employ_positions(
+#                 request_body=None
+#             )
