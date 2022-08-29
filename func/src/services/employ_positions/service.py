f78080e142daa3b6a5516150e8dc769f8062ae07
João Bonfim <joao.bonfim@lionx.com.br>
2022-08-29 15:31:59 -0300

    fix: endpoint return

--- func/src/services/employ_positions/service.py
+++ func/src/services/employ_positions/service.py
@@ -1,12 +1,12 @@
 # PROJECT IMPORTS
-from src.domain.models.employ_positions.response.model import EmployPositionsToResponse, EmployPositionsRecordResponse
+from src.domain.models.employ_positions.response.model import EmployPositionsToResponse
 from src.repositories.job_positions.repository import EmployPositionsRepository
 
 
 class EmployPositionsService:
 
     @classmethod
-    def get_employ_positions_response(cls) -> EmployPositionsRecordResponse:
+    def get_employ_positions_response(cls) -> list:
         employ_positions = EmployPositionsRepository.get_employ_positions()
 
         response = EmployPositionsToResponse.employ_positions_response(
