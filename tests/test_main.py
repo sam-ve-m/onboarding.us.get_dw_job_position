1b35345f7804ff588310457c0a19617f62be7cee
João Bonfim <joao.bonfim@lionx.com.br>
2022-09-06 08:30:57 -0300

    feat: added mutation test script and .coveragerc

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,7 @@ from werkzeug.datastructures import Headers
 # PROJECT IMPORTS
 from heimdall_client import Heimdall, HeimdallStatusResponses
 from func.main import get_employ_positions
+from func.src.domain.exceptions.exceptions import FailToFetchData
 from func.src.services.employ_positions.service import EmployPositionsService
 
 # STUB IMPORTS
@@ -46,3 +47,22 @@ async def test_when_sending_invalid_jwt_to_get_employ_positions_then_raise_error
             await get_employ_positions(
                 request_body=None
             )
+
+
+@pytest.mark.asyncio
+@patch.object(Heimdall, "decode_payload", return_value=(decoded_jwt_stub, HeimdallStatusResponses.SUCCESS))
+@patch.object(EmployPositionsService, "get_employ_positions_response")
+async def test_when_fail_to_fetch_data(
+    mock_get_response,
+    mock_decode_payload
+):
+    mock_get_response.side_effect = FailToFetchData()
+    app = Flask(__name__)
+    with app.test_request_context(
+            headers=Headers({"x-thebes-answer": "jwt_to_decode_stub"}),
+    ).request as request:
+        result = await get_employ_positions(
+            request_body=request
+        )
+    expected_result = b'{"result": false, "message": "Not able to get data from database", "success": false, "code": 100}'
+    assert result.data == expected_result
