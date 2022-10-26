5863d52712711d7d63bfd0aff77deb102f5d3153
Samuel V M <samuel.marcal@ligainvest.com.br>
2022-10-26 18:20:59 -0300

    feat: removed jwt validation

--- func/main.py
+++ func/main.py
@@ -1,65 +1,39 @@
 from http import HTTPStatus
 
 from etria_logger import Gladsheim
-from flask import request, Response, Request
 
 from src.domain.enums.status_code.enum import InternalCode
-from src.domain.exceptions.exceptions import ErrorOnDecodeJwt, FailToFetchData, InternalServerError
-from src.domain.models.jwt.response import Jwt
+from src.domain.exceptions.exceptions import FailToFetchData
 from src.domain.models.response.model import ResponseModel
-from src.services.employ_positions.service import EmployPositionsService
+from src.services.employ_position.service import EmployPositionService
 import flask
 
 
 async def get_employ_positions() -> flask.Response:
-
-    thebes_answer = flask.request.headers.get("x-thebes-answer")
-
     try:
-        jwt_data = Jwt(jwt=thebes_answer)
-        await jwt_data()
-        service_response = EmployPositionsService.get_employ_positions_response()
-
+        service_response = EmployPositionService.get_employ_position_response()
         response = ResponseModel(
             success=True,
-            code=InternalCode.SUCCESS.value,
+            code=InternalCode.SUCCESS,
             message="SUCCESS",
             result=service_response
         ).build_http_response(status=HTTPStatus.OK)
-        return response
-
-    except ErrorOnDecodeJwt as error:
-        Gladsheim.error(error=error, message=error.msg)
-        response = ResponseModel(
-            success=False,
-            code=InternalCode.JWT_INVALID.value,
-            message="Error On Decoding JWT"
-        ).build_http_response(status=HTTPStatus.UNAUTHORIZED)
-        return response
-
-    except TypeError as error:
-        Gladsheim.error(error=error, message=str(error))
-        response = ResponseModel(
-            success=False,
-            code=InternalCode.DATA_NOT_FOUND.value,
-            message="Data not found or inconsistent"
-        ).build_http_response(status=HTTPStatus.UNAUTHORIZED)
         return response
 
     except FailToFetchData as error:
         Gladsheim.error(error=error, message=error.msg)
         response = ResponseModel(
             success=False,
-            code=InternalCode.INTERNAL_SERVER_ERROR.value,
-            message="Not able to get data from database"
+            code=InternalCode.INTERNAL_SERVER_ERROR,
+            message="Not able to get or save data in database"
         ).build_http_response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
         return response
 
-    except (Exception, InternalServerError) as error:
+    except Exception as error:
         Gladsheim.error(error=error, message=str(error))
         response = ResponseModel(
             success=False,
-            code=InternalCode.INTERNAL_SERVER_ERROR.value,
+            code=InternalCode.INTERNAL_SERVER_ERROR,
             message="Something went wrong"
         ).build_http_response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
         return response
--- tests/test_main.py
+++ tests/test_main.py
@@ -14,42 +14,18 @@ with patch.object(RepositoryEnv, "__init__", return_value=None):
             with patch.object(logging.config, "dictConfig"):
                 from etria_logger import Gladsheim
                 from main import get_employ_positions
-                from src.domain.models.jwt.response import Jwt
                 from src.domain.enums.status_code.enum import InternalCode
                 from src.domain.models.response.model import ResponseModel
-                from src.domain.exceptions.exceptions import InternalServerError, ErrorOnDecodeJwt, FailToFetchData
-                from src.services.employ_positions.service import EmployPositionsService
+                from src.domain.exceptions.exceptions import FailToFetchData
+                from src.services.employ_position.service import EmployPositionService
 
 
-internal_server_error_case = (
-    InternalServerError("dummy"),
-    "dummy",
-    InternalCode.INTERNAL_SERVER_ERROR,
-    "Something went wrong",
-    HTTPStatus.INTERNAL_SERVER_ERROR
-)
-error_on_decode_jwt_case = (
-    ErrorOnDecodeJwt(),
-    ErrorOnDecodeJwt.msg,
-    InternalCode.JWT_INVALID,
-    "Error On Decoding JWT",
-    HTTPStatus.UNAUTHORIZED
-)
 fail_to_fetch_data_case = (
     FailToFetchData(),
     FailToFetchData.msg,
     InternalCode.INTERNAL_SERVER_ERROR,
-    "Not able to get data from database",
+    "Not able to get or save data in database",
     HTTPStatus.INTERNAL_SERVER_ERROR
-)
-
-
-typeerror_exception_case = (
-    TypeError("dummy"),
-    "dummy",
-    InternalCode.DATA_NOT_FOUND,
-    "Data not found or inconsistent",
-    HTTPStatus.UNAUTHORIZED
 )
 exception_case = (
     Exception("dummy"),
@@ -62,53 +38,44 @@ exception_case = (
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("exception,error_message,internal_status_code,response_message,response_status_code", [
-    internal_server_error_case,
-    error_on_decode_jwt_case,
     fail_to_fetch_data_case,
-    typeerror_exception_case,
     exception_case,
 ])
-@patch.object(EmployPositionsService, "get_employ_positions_response")
+@patch.object(EmployPositionService, "get_employ_position_response")
 @patch.object(Gladsheim, "error")
-@patch.object(Jwt, "__init__", return_value=None)
-@patch.object(Jwt, "__call__")
 @patch.object(ResponseModel, "__init__", return_value=None)
 @patch.object(ResponseModel, "build_http_response")
-async def test_get_employ_positions_raising_errors(
-            mocked_build_response, mocked_response_instance, mocked_jwt_decode,
-            mocked_jwt_instance, mocked_logger, mocked_service, monkeypatch,
+async def test_get_employ_position_raising_errors(
+            mocked_build_response, mocked_response_instance,
+            mocked_logger, mocked_service, monkeypatch,
             exception, error_message, internal_status_code, response_message, response_status_code,
 ):
     monkeypatch.setattr(flask, "request", MagicMock())
-    mocked_jwt_decode.side_effect = exception
+    mocked_service.side_effect = exception
     await get_employ_positions()
-    mocked_service.assert_not_called()
     mocked_logger.assert_called_once_with(error=exception, message=error_message)
     mocked_response_instance.assert_called_once_with(
         success=False,
         code=internal_status_code.value,
         message=response_message
     )
-    mocked_build_response.assert_called_once_with(status=response_status_code)
+    mocked_build_response.assert_called_once_with(position=response_status_code)
 
 
 dummy_response = "response"
 
 
 @pytest.mark.asyncio
-@patch.object(EmployPositionsService, "get_employ_positions_response", return_value=dummy_response)
+@patch.object(EmployPositionService, "get_employ_position_response", return_value=dummy_response)
 @patch.object(Gladsheim, "error")
-@patch.object(Jwt, "__init__", return_value=None)
-@patch.object(Jwt, "__call__")
 @patch.object(ResponseModel, "__init__", return_value=None)
 @patch.object(ResponseModel, "build_http_response", return_value=dummy_response)
-async def test_get_employ_positions(
-        mocked_build_response, mocked_response_instance, mocked_jwt_decode,
-        mocked_jwt_instance, mocked_logger, mocked_service, monkeypatch,
+async def test_get_employ_position(
+        mocked_build_response, mocked_response_instance,
+        mocked_logger, mocked_service, monkeypatch,
 ):
     monkeypatch.setattr(flask, "request", MagicMock())
     response = await get_employ_positions()
-    mocked_jwt_decode.assert_called()
     mocked_service.assert_called()
     mocked_logger.assert_not_called()
     mocked_response_instance.assert_called_once_with(
@@ -117,5 +84,5 @@ async def test_get_employ_positions(
         code=InternalCode.SUCCESS.value,
         message='SUCCESS',
     )
-    mocked_build_response.assert_called_once_with(status=HTTPStatus.OK)
+    mocked_build_response.assert_called_once_with(position=HTTPStatus.OK)
     assert dummy_response == response
